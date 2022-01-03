137ba23705002b66e9af974f28d63318b8527c38
Vítor Pereira <vmsapereira@gmail.com>
2022-01-03 15:00:15 +0000

    Aula 03/01/22

--- src/si/util/util.py
+++ src/si/util/util.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Vítor Pereira
+# Created Date: 01-09-2021
+# version ='0.0.1'
+# ---------------------------------------------------------------------------
+"""Utility module"""
+# ---------------------------------------------------------------------------
 import numpy as np
 
 # Y is reserved to idenfify dependent variables
@@ -62,3 +70,34 @@ def add_intersect(X):
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
+
+
+def to_categorical(y, num_classes=None, dtype='float32'):
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
+
+def minibatch(X, batchsize=256, shuffle=True):
+    N = X.shape[0]
+    ix = np.arange(N)
+    n_batches = int(np.ceil(N / batchsize))
+
+    if shuffle:
+        np.random.shuffle(ix)
+
+    def mb_generator():
+        for i in range(n_batches):
+            yield ix[i * batchsize: (i + 1) * batchsize]
+
+    return mb_generator(),
