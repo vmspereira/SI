cadd138bd5e16b9c7ee4f43398b4c3f0cfc5bf67
Vitor Pereira <vmsapereira@gmail.com>
2022-09-28 13:50:22 +0100

    Update __init__.py

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Vítor Pereira"
 __credits__ = ["Vítor Pereira"]
-__license__ = "GPL"
+__license__ = "Apache 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Vítor Pereira"
 __email__ = "vpereira@ceb.uminho.pt"
