167d4301e839f5fcce403e8aea13e2502da22634
Vitor Pereira <vmsapereira@gmail.com>
2023-08-30 14:15:26 +0100

    [ADD] Batch normalization

--- src/si/supervised/nn/nn.py
+++ src/si/supervised/nn/nn.py
@@ -7,89 +7,109 @@
 """Neural Network module"""
 # ---------------------------------------------------------------------------
 
+
 import numpy as np
 from abc import ABC, abstractmethod
-from si.supervised.model import Model
-from si.util import METRICS
+from copy import copy
 import warnings
+
+from si.supervised.model import Model
+from si.util import METRICS, minibatch
+from .optimizers import SGD
+
 
 class Layer(ABC):
     def __init__(self):
-        """ Abstract class for layers. 
+        """Abstract class for layers.
         A layer is a funtion that takes an input and produces an output.
-        The function may have learnable parameters, such as weights. 
+        The function may have learnable parameters, such as weights.
         """
         self.input = None
         self.output = None
 
     @abstractmethod
+    def initialize(self, optimizer):
+        raise NotImplementedError
+
+    @abstractmethod
     def forward(self, input):
-        """ Apply the layer 'function' to a given input
+        """Apply the layer 'function' to a given input
         returning an output.
         """
         raise NotImplementedError
 
     @abstractmethod
-    def backward(self, output_error, learning_rate):
-        """ The backward method allows to measure how each input or parameters
-            contributed to an error, such as, prediction errors. 
-     
-            This is achieved using derivatives: 
-            dE/dX tells us how much X contibuted to the error E.
+    def backward(self, output_error):
+        """The backward method allows to measure how each input or parameters
+        contributed to an error, such as, prediction errors.
 
-            Using the chain rule we can propagate errors across each layer 
-            or function:
-            
-            dE/dy = dE/dx * dx/dy
-            dE/dz = dE/dx * dx/dy * dy/dz
-            ...
+        This is achieved using derivatives:
+        dE/dX tells us how much X contibuted to the error E.
 
-            Knowing the contribution of a parameter to the final error, we 
-            can adjust the parameter. If w is a parameter whose contribution
-            to the final error is dE_total/dw, the value of w is adjusted to 
-            w = w - lr * dE_total/dw.
-            The learning rate (lr) controles the the learning speed... 
-            Note: learning too fast may lead to 'bad' learning, or not learning
-            what you should. 
+        Using the chain rule we can propagate errors across each layer
+        or function:
+
+        dE/dy = dE/dx * dx/dy
+        dE/dz = dE/dx * dx/dy * dy/dz
+        ...
+
+        Knowing the contribution of a parameter to the final error, we
+        can adjust the parameter. If w is a parameter whose contribution
+        to the final error is dE_total/dw, the value of w is adjusted to
+        w = w - lr * dE_total/dw.
+        The learning rate (lr) controles the the learning speed...
+        Note: learning too fast may lead to 'bad' learning, or not learning
+        what you should.
         """
         raise NotImplementedError
 
 
 class NN(Model):
-    def __init__(self, 
-                 epochs=1000, 
-                 lr=0.1, 
-                 verbose=True, 
-                 loss='MSE',
-                 metric = None,
-                 step=100):
-        """Neural Network model. The default loss function is the mean square error (MSE).
+    def __init__(
+        self,
+        epochs=1000,
+        batch_size=128,
+        optimizer=SGD(),
+        verbose=True,
+        loss="MSE",
+        metric=None,
+        step=100,
+    ):
+        """
+        Neural Network model. The default loss function is the mean square error (MSE).
         A NN may be regarded as a sequence of layers, functions applied sequentialy one after the other.
-     
+
         :param int epochs: Number of epochs.
-        :param float lr: The learning rate.
+        :param int batch_size: Minibach size
+        :param Optimizer optimizer: The optimizer.
         :param bool verbose: If all loss (and quality metric) are to be outputed. Default True.
-        :param str loss: The loss function. Default `MSE`. 
+        :param str loss: The loss function. Default `MSE`.
         :param callable metric: The quality metric. Default None.
         :param int step: the verbose steps. Default 100.
         """
         self.epochs = epochs
-        self.lr = lr
+        self.batch_size = batch_size
+        self.optimizer = optimizer
         self.verbose = verbose
         self.layers = []
-        
+
         if loss not in METRICS:
-            warnings.warn(f"{loss} is not a valid loss. Using MSE.")
-            loss = 'MSE'
+            warnings.warn(
+                f"{loss} is not a valid loss."
+                f"Available losses are {list(METRICS.keys())}."
+                "Using MSE."
+            )
+            loss = "MSE"
         self.loss = METRICS[loss][0]
         self.loss_prime = METRICS[loss][1]
-        
+
         self.metric = metric
         self.step = step
         self.is_fitted = False
 
     def add(self, layer):
         """Adds a layer to the network"""
+        layer.initialize(self.optimizer)
         self.layers.append(layer)
 
     def set_loss(self, loss):
@@ -101,9 +121,11 @@ class NN(Model):
             self.loss = METRICS[loss][0]
             self.loss_prime = METRICS[loss][1]
         else:
-            warnings.warn(f"{loss} is not a valid loss.")
-        
-        
+            warnings.warn(
+                f"{loss} is not a valid loss."
+                f"Available losses are {list(METRICS.keys())}."
+            )
+
     def set_metric(self, metric):
         self.metric = metric
 
@@ -111,41 +133,49 @@ class NN(Model):
         X, y = dataset.getXy()
         self.dataset = dataset
         self.history = dict()
-        for epoch in range(1, self.epochs+1):
+        for epoch in range(1, self.epochs + 1):
+            # lists to save the batch predicted and real values
+            # to be later used to compute the epoch loss and
+            # quality metrics
+            x_ = []
+            y_ = []
 
-            output = X
-            y_batch = y
-        
-            # forward propagation
-            # propagates values across all layers from
-            # the input to the final output.
-            
-            for layer in self.layers:
-                output = layer.forward(output)
-                
-            # backward propagation (propagates errors)
-            # Computes the derivatives to see how much each
-            # parameter contributed to the total error and adjusts
-            # the parameter acording to a defined learning rate
+            for batch in minibatch(X, y, self.batch_size):
+                output_batch, y_batch = batch
+                # forward propagation
+                # propagates values across all layers from
+                # the input to the final output.
+                for layer in self.layers:
+                    output_batch = layer.forward(output_batch)
 
-            error = self.loss_prime(y_batch, output)
-            for layer in reversed(self.layers):
-                error = layer.backward(error, self.lr)
-            
-            # calculates average error on all samples
-            err = self.loss(y_batch, output)
-            
+                # backward propagation (propagates errors)
+                # Computes the derivatives to see how much each
+                # parameter contributed to the total error and adjusts
+                # the parameter acording to a defined learning rate
+                error = self.loss_prime(y_batch, output_batch)
+                for layer in reversed(self.layers):
+                    error = layer.backward(error)
+
+                x_.append(output_batch)
+                y_.append(y_batch)
+
+            out = np.concatenate(x_)
+            y_all = np.concatenate(y_)
+            # compute the loss
+            err = self.loss(y_all, out)
+
             # if a quality metric is defined
             if self.metric is not None:
-                score = self.metric(y, output)
+                score = self.metric(y_all, out)
                 score_s = f" {self.metric.__name__}={score}"
             else:
                 score = 0
                 score_s = ""
-            
+
             self.history[epoch] = (err, score)
+
             # verbosity
-            if epoch%self.step==0:
+            if epoch % self.step == 0:
                 s = f"epoch {epoch}/{self.epochs} loss={err}{score_s}"
                 if self.verbose:
                     print(s)
@@ -168,6 +198,9 @@ class NN(Model):
         output = self.predict(X)
         return self.loss(y, output)
 
+    def __str__(self) -> str:
+        return "\n".join([str(layer) for layer in self.layers])
+
 
 class Dense(Layer):
     def __init__(self, input_size, output_size):
@@ -178,44 +211,48 @@ class Dense(Layer):
         :param input_size: the input size.
         :param output_size: the output size.
         """
-        # initialize weights from a 0 centered uniform distribution [-0.5, 0.5)
-        self.weights = np.random.rand(input_size, output_size) - 0.5
+        self.input_size = input_size
+        self.output_size = output_size
 
+    def initialize(self, optimizer):
+        # initialize weights from a 0 centered uniform distribution [-0.5, 0.5)
+        self.weights = np.random.rand(self.input_size, self.output_size) - 0.5
         # initialing biases
-        self.bias = np.zeros((1, output_size))
+        self.bias = np.zeros((1, self.output_size))
+        self.w_opt = copy(optimizer)
+        self.b_opt = copy(optimizer)
 
     def forward(self, input_data):
         self.input = input_data
         self.output = np.dot(self.input, self.weights) + self.bias
         return self.output
 
-    def backward(self, output_error, learning_rate):
-        """ Here is where the magic happens!
+    def backward(self, output_error):
+        """Here is where the magic happens!
 
         Computes the dE/dW, dE/dB for a given output_error=dE/dY.
-        
+
         Returns input_error=dE/dX to feed the previous layer.
         """
         # computes the weight error: dE/dW = X.T * dE/dY
         weights_error = np.dot(self.input.T, output_error)
-        
         # computes the bias error: dE/dB = dE/dY
         bias_error = np.sum(output_error, axis=0)
-        
-        # computes the layer input error (the output error from the previous layer), 
+        # computes the layer input error (the output error from the previous layer),
         # dE/dX, to pass on to the previous layer
         input_error = np.dot(output_error, self.weights.T)
-        
-        # updates the parameters accordind to a defined learning rate
-        self.weights -= learning_rate * weights_error
-        self.bias -= learning_rate * bias_error
+
+        # updates the parameters according to a defined optimizer
+        self.weights = self.w_opt.update(self.weights, weights_error)
+        self.bias = self.b_opt.update(self.bias, bias_error)
+
         return input_error
 
     def __str__(self):
-        return "Dense"
+        return f"Dense {self.weights.shape}"
 
     def set_weights(self, weights, bias):
-        """ Sets the weights and bias of the 
+        """Sets the weights and bias of the
         layer.
 
         :params weights: A numpy array of weight values
@@ -227,13 +264,17 @@ class Dense(Layer):
 
 class Flatten(Layer):
     """A flatten layer,flattens all but the 1st dimention."""
+
     def forward(self, input):
         self.input_shape = input.shape
         # flattens all but the 1st dimention
         output = input.reshape(input.shape[0], -1)
         return output
 
-    def backward(self, output_error, learning_rate):
+    def initialize(self, optimizer):
+        pass
+
+    def backward(self, output_error):
         return output_error.reshape(self.input_shape)
 
     def __str__(self):
@@ -241,21 +282,23 @@ class Flatten(Layer):
 
 
 class Dropout(Layer):
-
     def __init__(self, prob=0.5):
-        """A dropout layer. 
+        """A dropout layer.
         :param (float) prob: The dropout probability. Defaults to 0.5.
         """
         self.prob = prob
+
+    def initialize(self, optimizer):
+        pass
 
     def forward(self, input):
         self.mask = np.random.binomial(1, self.prob, size=input.shape) / self.prob
         out = input * self.mask
         return out.reshape(input.shape)
 
-    def backward(self, output_error, learning_rate):
+    def backward(self, output_error):
         dX = output_error * self.mask
         return dX
-    
+
     def __str__(self):
         return f"DropOut {self.prob}"
